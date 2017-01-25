31d14e0add9c70eda72c631b45eadbc24eab058f
Simon Fridlund <simon@fridlund.email>
2017-01-25 23:26:55 +0100

    Update dependencies
    Use spacesbars and templating-runtime

--- package.js
+++ package.js
@@ -10,8 +10,9 @@ Package.onUse(function onUse(api) {
   api.versionsFrom('1.4.2.2');
 
   api.use([
-    'blaze',
     'ecmascript',
+    'spacebars@1.0.13',
+    'templating-runtime@1.2.15',
   ]);
 
   api.mainModule('client/helpers.js', 'client', { lazy: true });
